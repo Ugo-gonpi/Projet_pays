9bbc291bc7e133012acc952ff98bfa918889f824
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-03-07 14:24:52 +0100

    mit le useEffect et le lein vers l'api, plus cree le debut des cards de page d'acceuil

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+import Header from './components/Header';
 
 function App() {
 
-  return (
-    <div className='w-screen h-screen'>
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all")
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className='w-full h-full flex justify-center items-start flex-wrap bg-white'>
+      <Header />
+      {data.map((element, key) => {
+        return(
+          <div key={key} className='w-[200px] h-[240px] flex justify-center items-center flex-wrap flex-row mb-[10px] text-center ml-[10px] mr-[10px] mt-[10px] rounded-[10px] bg-[hsl(0,0%,95%)]'>
+            <img src={element.flags.png} alt="" className='w-full h-[50%] rounded-t-lg'/>
+            <p className='w-full h-[50%]'>
+              {element.name.common}
+            </p>
+          </div>
+        )
+      })}
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='w-screen h-[60px] bg-black'>
+            
+        </div>
+    );
+}
+
+export default Header;
