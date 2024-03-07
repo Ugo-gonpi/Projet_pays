db05b1dd4ec0cf1f4be02b641ada09c6b0f210af
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-03-07 16:12:51 +0100

    aired out the code

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,34 @@ function App() {
   return (
     <div className='w-full h-full flex justify-center items-start flex-wrap bg-white'>
       <Header />
+
       <SearchBar />
+
       {data.map((element, key) => {
         return(
           <div key={key} className='w-[300px] h-[340px] flex justify-center items-center flex-wrap flex-row mb-[10px] text-center ml-[10px] mr-[10px] mt-[10px] rounded-[10px] bg-[hsl(0,0%,95%)] hover:scale-105'>
+
             <img src={element.flags.png} alt="" className='w-full h-[50%] rounded-t-lg'/>
+
             <div className='w-full h-[50%] font-[NunitoSans] font-bold text-[20px] text-center'>
+
               <h3 className='text-[hsl(200,15%,8%)] font-extrabold underline text-[24px]'>
                 {element.name.common}
               </h3>
+
               <p className='text-black text-[17px]'>Population: <span className='text-[hsl(198,26%,17%)]'>{element.population}</span></p>
+
               <p className='text-black text-[17px]'>Region: <span className='text-[hsl(198,26%,17%)]'>{element.region}</span></p>
+
               <p className='text-black text-[17px]'>Capital: <span className='text-[hsl(198,26%,17%)]'>{element.capital}</span></p>
+
             </div>
+
           </div>
         )
+
       })}
+      
     </div>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,20 +6,32 @@ const SearchBar = () => {
         <div className='w-screen h-[100px] flex items-center justify-around'>
 
             <label className="input input-bordered flex items-center gap-2 w-[400px] mt-4 bg-gray-100">
+
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+
                 <input type="text" className="grow text-black" placeholder="Search for a country..." />
+                
             </label>
 
             <details className="dropdown flex items-center mt-[10px]">
+
                 <summary className="m-1 btn bg-slate-200 text-slate-700">Filter by Region</summary>
+
                 <ul className="p-2 shadow menu dropdown-content z-[1] rounded-[10px] w-52 bg-slate-200">
                     <li><a className='text-black'>Africa</a></li>
+
                     <li><a className='text-black'>America</a></li>
+
                     <li><a className='text-black'>Asia</a></li>
+
                     <li><a className='text-black'>Europe</a></li>
+
                     <li><a className='text-black'>Oceania</a></li>
+
                 </ul>
+
             </details>
+
         </div>
     );
 }
